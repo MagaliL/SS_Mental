26d84cb28368f6c1a15b271a4ac6a9b49562c641
Magali L. Espinosa <magalizethe@gmail.com>
2025-03-30 23:37:03 -0600

    Descripción de los cambios

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 
 <template>
+<div class="container">
     <div class="layout-footer">
-        
        <!-- Se añadieron los logos de la facultad y de la universidad al footer (Magali) -->
        <!-- class logos es para poder modificar los logos en el ccs (Magali)-->
         <div class="footerlogos">
@@ -18,4 +18,5 @@
             </div>
         </div>
     </div>
+</div>
 </template>
