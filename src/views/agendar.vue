be42bc57b0c163e3a9dedbcf6264b0e16ded8c6e
Magali L. Espinosa <magalizethe@gmail.com>
2025-04-08 11:17:00 -0600

    cambios no guardados de un texto en agendar

--- src/views/agendar.vue
+++ src/views/agendar.vue
@@ -1,13 +1,13 @@
 <script setup>
 // Esta parte es para indexarlo con la página princial
-import AppLayout from '@/layout/AppLayout.vue';
 </script>
-
 
 <template>
     <div class="about">
-      <h1>agendar</h1>
+      <h1>Esta es la página "Agendar"</h1>
       <!-- Enlace de vuelta al home -->
+
+      
       <router-link to="/" class="nav-link">Volver al inicio</router-link>
     </div>
   </template>
