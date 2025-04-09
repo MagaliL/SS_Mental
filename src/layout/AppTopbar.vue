5746732a82739ec6ec9759aa1a7540b6522dfebf
Mikpo31 <miguel.pavon552b@gmail.com>
2025-04-08 22:30:06 -0600

    Modifique la topbar y los botones en dashboard

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -1,42 +1,39 @@
 <script setup>
-import { useLayout } from '@/layout/composables/layout';
+//import { useLayout } from '@/layout/composables/layout';
 
-const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
+//const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
 </script>
 
 <template>
     <div class="layout-topbar">
         <div class="layout-topbar-logo-container">
+            <!-- No lo borro por si llegamos a usarlo
             <button class="layout-menu-button layout-topbar-action" @click="toggleMenu">
                 <i class="pi pi-bars"></i>
-            </button>
-            <router-link to="/" class="layout-topbar-logo">
-                <img src="/demo/images/logos/top_bar.png" alt="Logo" class="h-10" />
+            </button> -->
+            <router-link to="/" class="layout-topbar-logo p-6">
+                <img src="/demo/images/logos/top_bar.png" alt="Logo" class="h-15" />
             </router-link>
         </div>
 
         <!-- Menú de Navegación -->
-        <nav class="hidden lg:flex space-x-20">
-            <router-link to="/" class="text-blue-600 font-semibold">Inicio</router-link>
-            <router-link to="/emergencia" class="text-blue-900 font-semibold">Emergencia</router-link>
-            <router-link to="/agendar" class="text-blue-900 font- semibold">Agendar primera cita</router-link>
+        <nav class="hidden lg:flex mx-auto space-x-10 items-center h-full">
+            <!-- Añade items-center y h-full -->
+            <router-link to="/" class="text-blue-600 font-semibold py-2">Inicio</router-link>
+            <router-link to="/emergencia" class="text-blue-900 font-semibold py-2">Emergencia</router-link>
+            <!--router-link to="/agendar" class="text-blue-900 font-semibold py-2">Agendar primera cita</router-link-->
+            <a href="https://forms.gle/MLKdKxPMz7szFCiq5" class="text-blue-900 font-semibold py-2"> Agendar primera cita </a>
             <div class="relative">
-                <button class="text-blue-900 font-semibold">Más ▼</button>
-                <!-- Aquí puedes agregar un dropdown si es necesario -->
+                <button class="text-blue-900 font-semibold py-2">Más ▼</button>
             </div>
         </nav>
 
         <!-- Configuración y Horario -->
         <div class="layout-topbar-actions flex items-center space-x-4">
-            <div class="layout-config-menu flex items-center space-x-3"></div>
+            <div class="layout-config-menu flex items-center space-x-4"></div>
 
-            <!-- Horario de Atención -->
-            <div class="bg-blue-50 text-blue-900 px-4 py-2 rounded-full text-sm font-bold hidden lg:block">
-                Acceso
-                <br />
-                <span class="text-xs font-normal"></span>
-            </div>
-
+            <!-- Boton de acceso -->
+            <router-link to="/login" class="bg-blue-50 hover:bg-blue-100 text-blue-900 px-4 py-2 rounded-full text-sm font-bold hidden lg:block transition-colors"> Acceso </router-link>
             <!-- Menú Responsive -->
             <button
                 class="layout-topbar-menu-button layout-topbar-action lg:hidden"
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,126 +1,127 @@
 <script setup>
-
-
 import Galleria from 'primevue/galleria';
 
-
-
 // Contenido personalizado aquí:
-const welcomeMessage = "Bienvenido";
+const welcomeMessage = 'Bienvenido';
 </script>
-
-
 
 <!-- Html del homepage, sus estilos están en _main.scss -->
 
 <template>
+    <div class="Homepage_container">
+        <div class="columna">
+            <section class="hero-section">
+                <center><h1 class="titulo_home">Estamos para escucharte</h1></center>
+                <center><h2 class="subtitulo_home">Acompañamiento a tu salud mental</h2></center>
+            </section>
+            <div class="Info">
+                <center>
+                    <b><p>Horario de atención del programa</p></b> <b><p>De lunes a viernes de 9:00 a 19:00</p></b>
+                </center>
+            </div>
+            <br /><br />
 
-<div class="Homepage_container">
-  
+            <!-- Botones -->
 
-    <div class="columna">
+            <div class="Botones_home">
+                <!-- Botón AGENDAR PRIMERA CITA (enlace externo) -->
+                <a
+                    href="https://forms.gle/ewEaMeabtZN2pbBU6"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="primary-button1 inline-flex justify-center items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                >
+                    AGENDAR PRIMERA CITA
+                </a>
+                <br />
 
-    <section class="hero-section">
-      <center><h1 class="titulo_home">Estamos para escucharte</h1></center>
-      <center><h2 class="subtitulo_home">Acompañamiento a tu salud mental</h2></center>
-    </section>
-    <div class="Info">  
-      <center><b><p>Horario de atención del programa</p></b>
-      <b><p>De lunes a viernes de 9:00 a 19:00</p></b></center>
-    </div><br><br>
-    
-    <!-- Botones -->
+                <!-- Botones originales (para comparación) -->
+                <router-link to="/emergencia" custom v-slot="{ navigate }">
+                    <button
+                        class="primary-button2 inline-flex justify-center items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                        @click="navigate"
+                    >
+                        EMERGENCIA
+                    </button>
+                </router-link>
+                <br />
 
-    <div class="Botones_home">
-      <router-link to="/agendar" custom v-slot="{ navigate }">
-      <button class="primary-button1" @click="navigate">AGENDAR PRIMERA CITA</button>
-      </router-link><br>
-
-      <router-link to="/emergencia" custom v-slot="{ navigate }">
-      <button class="primary-button2" @click="navigate">EMERGENCIA</button>
-      </router-link> <br>
-      
-      <router-link to="/actividades" custom v-slot="{ navigate }">
-      <button class="primary-button3" @click="navigate">ACTIVIDADES</button>  
-      </router-link><br>
-    </div>
-
-    </div>
-
-    <!-- Hasta aquí llega la primera columna  -->
-
-    <div class="columna">
-    <!-- Redes sociales -->
-
-    <div class="redes_sociales">
-        <center><b><h4 class="Info">Síguenos en Facebook</h4></b></center>
-        <center><button class="primary-button2"><a href="https://www.facebook.com/profile.php?id=61572265868700" target="_blank">NUESTRO FACEBOOK</a></button></center>
-    </div>
-    <br><br><br>
-
-   <center> <img class="imagen1" src="/public/demo/images/home/PSMFI-LOG.png" alt="Programa de Salud Mental de la Facultad de Ingeniería"></center>
-    </div>  
-
-    <br><br><br>
-   
-</div>
-
-<div class="slideshow">
- 
- <!-- Componente slideshow -->
-
- <Galleria
-    ref="galleria"
-    v-model:activeIndex="activeIndex"
-    :value="images"
-    :numVisible="5"
-    containerStyle="max-width: 640px"
-    :showThumbnails="showThumbnails"
-    :showItemNavigators="true"
-    :showItemNavigatorsOnHover="true"
-    :circular="true"
-    :autoPlay="isAutoPlay"
-    :transitionInterval="3000"
-    :responsiveOptions="responsiveOptions"
-    :pt="{
-        root: {
-            class: [{ 'flex flex-col': fullScreen }]
-        },
-        content: {
-            class: ['relative', { 'flex-1 justify-center': fullScreen }]
-        },
-        thumbnails: 'absolute w-full left-0 bottom-0'
-    }"
->
-    <template #item="slotProps">
-        <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" :style="[{ width: !fullScreen ? '100%' : '', display: !fullScreen ? 'block' : '' }]" />
-    </template>
-    <template #thumbnail="slotProps">
-        <div class="grid gap-4 justify-center">
-            <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" style="display: block" />
+                <router-link to="/actividades" custom v-slot="{ navigate }">
+                    <button
+                        class="primary-button3 inline-flex justify-center items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                        @click="navigate"
+                    >
+                        ACTIVIDADES
+                    </button>
+                </router-link>
+            </div>
         </div>
-    </template>
-    <template #footer>
-        <div class="flex items-stretch bg-surface-950 text-white h-10">
-            <button type="button" @click="onThumbnailButtonClick" class="bg-transparent border-none rounded-none hover:bg-white/10 text-white inline-flex justify-center items-center cursor-pointer px-3">
-                <i class="pi pi-th-large"></i>
-            </button>
-            <button type="button" @click="toggleAutoSlide" class="bg-transparent border-none rounded-none hover:bg-white/10 text-white inline-flex justify-center items-center cursor-pointer px-3"><i :class="slideButtonIcon"></i></button>
-            <span v-if="images" class="flex items-center gap-4 ml-3">
-                <span class="text-sm">{{ activeIndex + 1 }}/{{ images.length }}</span>
-                <span class="font-bold text-sm">{{ images[activeIndex].title }}</span>
-                <span class="text-sm">{{ images[activeIndex].alt }}</span>
-            </span>
-            <button type="button" @click="toggleFullScreen" class="bg-transparent border-none rounded-none hover:bg-white/10 text-white inline-flex justify-center items-center cursor-pointer px-3 ml-auto">
-                <i :class="fullScreenIcon"></i>
-            </button>
+
+        <!-- Hasta aquí llega la primera columna  -->
+
+        <div class="columna">
+            <!-- Redes sociales -->
+
+            <div class="redes_sociales">
+                <center>
+                    <b><h4 class="Info">Síguenos en Facebook</h4></b>
+                </center>
+                <center>
+                    <button class="primary-button2"><a href="https://www.facebook.com/profile.php?id=61572265868700" target="_blank">NUESTRO FACEBOOK</a></button>
+                </center>
+            </div>
+            <br /><br /><br />
+
+            <center><img class="imagen1" src="/public/demo/images/home/PSMFI-LOG.png" alt="Programa de Salud Mental de la Facultad de Ingeniería" /></center>
         </div>
-    </template>
-</Galleria>
 
+        <br /><br /><br />
+    </div>
 
-</div> 
+    <div class="slideshow">
+        <!-- Componente slideshow -->
 
+        <Galleria
+            ref="galleria"
+            v-model:activeIndex="activeIndex"
+            :value="images"
+            :numVisible="5"
+            containerStyle="max-width: 640px"
+            :showThumbnails="showThumbnails"
+            :showItemNavigators="true"
+            :showItemNavigatorsOnHover="true"
+            :circular="true"
+            :autoPlay="isAutoPlay"
+            :transitionInterval="3000"
+            :responsiveOptions="responsiveOptions"
+            :pt="{ root: { class: [{ 'flex flex-col': fullScreen }] }, content: { class: ['relative', { 'flex-1 justify-center': fullScreen }] }, thumbnails: 'absolute w-full left-0 bottom-0' }"
+        >
+            <template #item="slotProps">
+                <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" :style="[{ width: !fullScreen ? '100%' : '', display: !fullScreen ? 'block' : '' }]" />
+            </template>
+            <template #thumbnail="slotProps">
+                <div class="grid gap-4 justify-center">
+                    <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" style="display: block" />
+                </div>
+            </template>
+            <template #footer>
+                <div class="flex items-stretch bg-surface-950 text-white h-10">
+                    <button type="button" @click="onThumbnailButtonClick" class="bg-transparent border-none rounded-none hover:bg-white/10 text-white inline-flex justify-center items-center cursor-pointer px-3">
+                        <i class="pi pi-th-large"></i>
+                    </button>
+                    <button type="button" @click="toggleAutoSlide" class="bg-transparent border-none rounded-none hover:bg-white/10 text-white inline-flex justify-center items-center cursor-pointer px-3"><i :class="slideButtonIcon"></i></button>
+                    <span v-if="images" class="flex items-center gap-4 ml-3">
+                        <span class="text-sm">{{ activeIndex + 1 }}/{{ images.length }}</span>
+                        <span class="font-bold text-sm">{{ images[activeIndex].title }}</span>
+                        <span class="text-sm">{{ images[activeIndex].alt }}</span>
+                    </span>
+                    <button type="button" @click="toggleFullScreen" class="bg-transparent border-none rounded-none hover:bg-white/10 text-white inline-flex justify-center items-center cursor-pointer px-3 ml-auto">
+                        <i :class="fullScreenIcon"></i>
+                    </button>
+                </div>
+            </template>
+        </Galleria>
+    </div>
 
-<button @click="$vueFlux.show('next')">NEXT</button>
+    <button @click="$vueFlux.show('next')">NEXT</button>
 </template>
